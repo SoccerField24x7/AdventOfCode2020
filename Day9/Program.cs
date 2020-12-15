67c61f85be8db48a736f827d1f1c629ec2db80ee
Jesse Quijano <jesse@quijano.net>
2020-12-14 19:13:37 -0800

    complete part 1 and part 2

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Advent2020.Helpers;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Day9
 {
@@ -11,9 +12,54 @@ namespace Day9
             var dataList = FileHelper.GetFileContents<double>("./data/data.txt");
 
             // outer loop looking for non-conformists
-            var result = HasSum(dataList, 0);
+            int i;
+            double target = 0;
+            for (i=25; i < dataList.Count; i++)
+            {
+                List<double> subset = new List<double>();
+                subset = dataList.GetRange(i-25, 25);
+                target = dataList[i];
+                var result = HasSum(subset, target);
+                if (!result)
+                {
+                    // no gots!
+                    Console.WriteLine($"The first failing number is {target} at position {i}");
+                    break;
+                }
+            }
 
-            Console.WriteLine("Hello World!");
+            var contig = FindContiguousRun(dataList, target);
+            Console.WriteLine($"The encryption weakness is {contig.Min() + contig.Max()}");
+        }
+
+        public static List<double> FindContiguousRun(List<double> dataList, double targetValue)
+        {
+            int i, highWaterMark = 0, inner;
+            double thisSum;
+            List<double> contig = new List<double>();
+
+            for (i = highWaterMark; i < dataList.Count; i++)
+            {
+                thisSum = 0;
+                for (inner=highWaterMark; inner < dataList.Count ; inner++) // need a better high bound
+                {
+                    thisSum += dataList[inner];
+
+                    if (thisSum >= targetValue)
+                    {
+                        break;
+                    }
+                }
+
+                if (thisSum == targetValue)
+                {
+                    return dataList.GetRange(highWaterMark, inner - highWaterMark + 1);
+                }
+
+                highWaterMark++;
+            }
+
+            return contig;
         }
 
         public static bool HasSum(List<double> dataList, double targetValue)
