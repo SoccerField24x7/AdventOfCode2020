d6e54c077fa9ced2d4d1c4da6cb462cbaa2075ab
Jesse Quijano <jesse@quijano.net>
2021-10-24 20:25:49 -0700

    part 1 final

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,55 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Advent2020.Helpers;
 
 namespace Day11
 {
     class Program
     {
-        public static readonly string OCCUPIED = "#";
-        public static readonly string OPEN = "L";
-        public static readonly string FLOOR = ".";
+        public static readonly char OCCUPIED = '#';
+        public static readonly char OPEN = 'L';
+        public static readonly char FLOOR = '.';
 
         static void Main(string[] args)
         {
-            var dataList = FileHelper.GetFileContents<string>("./data/sample.txt");
+            var currentSeating = FileHelper.GetFileContents<string>("./data/data.txt");
 
-            // If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
-            // If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
-            // Otherwise, the seat's state does not change.
+            List<string> newSeating = new();
+            List<string> lastSeating = new();
+            string evaluatedRow;
 
-            // int evaluatingRow = 0;
-            List<string> newSeatingChart = new();
-            string result;
-
-            for (var i=0; i < dataList.Count; i++) // start at 1 because we need the second row to determine seating for the first
+            while (true)
             {
-                // conditional selection of rows for evaluation
-                if (i == 0)
+                for (var i=0; i < currentSeating.Count; i++)
                 {
-                   result = EvaluateRow(dataList.GetRange(i, 2), true, false);
-                }
-                else if (i + 1 == dataList.Count)
-                {
-                    result = EvaluateRow(dataList.GetRange(i-1, 2), false, true);
-                }
-                else
-                {
-                    result = EvaluateRow(dataList.GetRange(i-1, 3), false, false);
+                    // conditional selection of rows for evaluation: 2 for first and last rows, 3 for all middle evals
+                    if (i == 0)
+                    {
+                    evaluatedRow = PadRowsIfNecessaryAndEvaluate(currentSeating.GetRange(i, 2), true, false);
+                    }
+                    else if (i + 1 == currentSeating.Count)
+                    {
+                        evaluatedRow = PadRowsIfNecessaryAndEvaluate(currentSeating.GetRange(i-1, 2), false, true);
+                    }
+                    else
+                    {
+                        evaluatedRow = PadRowsIfNecessaryAndEvaluate(currentSeating.GetRange(i-1, 3), false, false);
+                    }
+
+                    newSeating.Add(evaluatedRow);
                 }
 
-                newSeatingChart.Add(result);
+                if (newSeating.SequenceEqual(lastSeating))
+                {
+                    break;
+                }
+
+                // reset: make current seating chart the latest evaluation
+                currentSeating.Clear();
+                currentSeating.AddRange(newSeating);
+                
+                // reset: make the last seating chart the latest evaluation
+                lastSeating.Clear();
+                lastSeating.AddRange(newSeating);
+
+                // finally, reset the new seating chart to prepare for next eval
+                newSeating.Clear();
             }
 
-            Console.WriteLine("*********************");
-
-            foreach (string row in newSeatingChart)
+            // count the overall results
+            int count = 0;
+            foreach (var row in lastSeating)
             {
-                Console.WriteLine(row);
+                count += row.Length - row.Replace(OCCUPIED.ToString(), "").Length;
             }
+
+            Console.WriteLine(count);
         }
 
-        public static string EvaluateRow(List<string> rows, bool firstBlock, bool lastBlock)
+        public static string PadRowsIfNecessaryAndEvaluate(List<string> rows, bool firstBlock, bool lastBlock)
         {
             if (rows.Count > 3 || rows.Count < 2)
             {
@@ -61,13 +79,6 @@ namespace Day11
                 throw new Exception("Can't be the first and the last evaluation block.");
             }
 
-            // foreach (var row in rows)
-            // {
-            //     Console.WriteLine(row);
-            // }
-
-            // Console.WriteLine("-");
-
             if (firstBlock)
             {
                 rows.Insert(0, new string('.', rows[1].Length));
@@ -76,41 +87,68 @@ namespace Day11
             {
                 rows.Add(new string('.', rows[1].Length));
             }
-            else
-            {
-                //result = EvaluateBlock(dataList.GetRange(i-1, 3), false, false);
-            }
 
             return GetNewSeatAssignmentsForRow(rows);
         }
 
         public static string GetNewSeatAssignmentsForRow(List<string> rows)
         {
-            string result;
-            for (var i=0; i < rows.Count; i++)
+            string result = string.Empty;
+            
+            var thisRow = rows[1];
+            var aboveRow = rows[0];
+            var belowRow = rows[2];
+
+            for (var i=0; i < rows[1].Length; i++)
             {
-                if (i == 0)
+                List<char> seatBlock = new();
+                var evalSeat = rows[1][i];
+
+                seatBlock.Add(aboveRow[i]);
+                seatBlock.Add(belowRow[i]);
+
+                // pad the far right and left positions with Z (out of bounds)
+                seatBlock.Add(i == thisRow.Length - 1 ? 'Z' : aboveRow[i+1]);
+                seatBlock.Add(i == thisRow.Length - 1 ? 'Z' : thisRow[i+1]);
+                seatBlock.Add(i == thisRow.Length - 1 ? 'Z' : belowRow[i+1]);
+
+                seatBlock.Add(i == 0 ? 'Z' : aboveRow[i-1]);
+                seatBlock.Add(i == 0 ? 'Z' : thisRow[i-1]);
+                seatBlock.Add(i == 0 ? 'Z' : belowRow[i-1]);
+
+                if (evalSeat == OPEN)
                 {
-                    if (rows[1][i] == OPEN)
+                    // we can get cute later, for now just remove the garbage squares
+                    var cleanBlock = seatBlock.Where(x => x != 'Z' && x != '.').ToList();
+
+                    if (cleanBlock.All(x => x == OPEN))
                     {
-                        rows[0][i] == OPEN || 
+                        result += OCCUPIED; // If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
                     }
                     else
                     {
-                        result += OPEN; // move this to outside the ifs
+                        result += OPEN;  // Otherwise, the seat's state does not change.
                     }
                 }
-                else if (i + 1 == rows.Count)
+                else if (evalSeat == FLOOR)
                 {
-
+                    result += FLOOR;
                 }
                 else
                 {
-                    
+                    // If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
+                    if (seatBlock.Where(x => x == OCCUPIED).Count() > 3)
+                    {
+                        result += OPEN;
+                    }
+                    else
+                    {
+                        result += evalSeat;
+                    }
                 }
             }
 
-            return "";
+            return result;
         }
     }
 }
