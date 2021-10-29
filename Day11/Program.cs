a758f02b75849c21e685ecfb9b74a2c1b99b900b
Jesse Quijano <jesse@quijano.net>
2021-10-29 14:44:34 -0700

    final part 2 solution

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -66,13 +66,84 @@ namespace Day11
 
             Console.WriteLine(count);
 
-            currentSeating = FileHelper.GetFileContents<string>("./data/sample.txt");
+            // Reset everything for Part II
+            currentSeating = FileHelper.GetFileContents<string>("./data/data.txt");
+            lastSeating.Clear();
+            newSeating.Clear();
+            count = 0;
 
-            char result = LookUp(currentSeating, 0, 0);
-            
-            result = LookUp(currentSeating, 7, 9);
+            while (true)
+            {
+                for (var i=0; i < currentSeating.Count; i++)
+                {
+                    evaluatedRow = string.Empty;
+                    for (var x=0; x < currentSeating[i].Length; x++)
+                    {
+                        var viewedFromSeat = LookAroundMe(currentSeating, i, x);
 
-            result = LookUp(currentSeating, 1, 9);
+                        evaluatedRow += EvaluateSeat(viewedFromSeat, currentSeating[i][x], 5);
+                    }
+
+                    newSeating.Add(evaluatedRow);
+                }
+
+                if (newSeating.SequenceEqual(lastSeating))
+                {
+                    break;
+                }
+
+                // reset: make current seating chart the latest evaluation
+                currentSeating.Clear();
+                currentSeating.AddRange(newSeating);
+                
+                // reset: make the last seating chart the latest evaluation
+                lastSeating.Clear();
+                lastSeating.AddRange(newSeating);
+
+                // finally, reset the new seating chart to prepare for next eval
+                newSeating.Clear();
+            }
+
+            foreach (var row in lastSeating)
+            {
+                count += row.Length - row.Replace(OCCUPIED.ToString(), "").Length;
+            }
+
+            Console.WriteLine(count);
+        }
+
+        public static char EvaluateSeat(List<char> seatsAroundMe, char thisSeatStatus, int surroundingSeatLimit = 4)
+        {
+            if (thisSeatStatus == OPEN)
+            {
+                // we can get cute later, for now just remove the garbage squares
+                var cleanBlock = seatsAroundMe.Where(x => x != 'Z' && x != '.').ToList();
+
+                if (cleanBlock.All(x => x == OPEN))
+                {
+                    return OCCUPIED; // If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
+                }
+                else
+                {
+                    return OPEN;  // Otherwise, the seat's state does not change.
+                }
+            }
+            else if (thisSeatStatus == FLOOR)
+            {
+                return FLOOR;
+            }
+            else
+            {
+                // If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
+                if (seatsAroundMe.Where(x => x == OCCUPIED).Count() >= surroundingSeatLimit)
+                {
+                    return OPEN;
+                }
+                else
+                {
+                    return thisSeatStatus;
+                }
+            }
         }
 
         public static string PadRowsIfNecessaryAndEvaluate(List<string> rows, bool firstBlock, bool lastBlock)
@@ -99,14 +170,27 @@ namespace Day11
             return GetNewSeatAssignmentsForRow(rows);
         }
 
-        // public static string EvaluateRow(List<string> layout, int row)
-        // {
+        public static List<char> LookAroundMe(List<string> layout, int row, int col)
+        {
+            // what can I see from this seat?
+            List<char> viewedFromSeat = new();
+            viewedFromSeat.Add(LookUp(layout, row, col));
+            viewedFromSeat.Add(LookDown(layout, row, col));
+            viewedFromSeat.Add(LookLeft(layout, row, col));
+            viewedFromSeat.Add(LookUpLeft(layout, row, col));
+            viewedFromSeat.Add(LookDownLeft(layout, row, col));
+            viewedFromSeat.Add(LookRight(layout, row, col));
+            viewedFromSeat.Add(LookUpRight(layout, row, col));
+            viewedFromSeat.Add(LookDownRight(layout, row, col));
 
-        // }
+            return viewedFromSeat;
+        }
 
         public static char LookUp(List<string> layout, int row, int col)
         {
-            if (row == 0)
+            int BOUNDRY = 0;
+
+            if (row == BOUNDRY)
             {
                 return 'Z';
             }
@@ -118,7 +202,55 @@ namespace Day11
                 continue;
             }
 
-            if (i == -1)
+            if (i == BOUNDRY - 1)
+            {
+                return 'Z';
+            }
+
+            return layout[i][col];
+        }
+
+        public static char LookDown(List<string> layout, int row, int col)
+        {
+            int BOUNDRY = layout.Count - 1;
+
+            if (row == BOUNDRY)
+            {
+                return 'Z';
+            }
+
+            int i = row;
+
+            while (++i <= BOUNDRY && layout[i][col] != OPEN && layout[i][col] != OCCUPIED)
+            {
+                continue;
+            }
+
+            if (i == BOUNDRY + 1)
+            {
+                return 'Z';
+            }
+
+            return layout[i][col];
+        }
+
+        public static char LookLeft(List<string> layout, int row, int col)
+        {
+            int BOUNDRY = 0;
+
+            if (col == BOUNDRY)
+            {
+                return 'Z';
+            }
+
+            int i = col;
+
+            while (--i >= BOUNDRY && layout[row][i] != OPEN && layout[row][i] != OCCUPIED)
+            {
+                continue;
+            }
+
+            if (i == BOUNDRY - 1)
             {
                 return 'Z';
             }
@@ -126,16 +258,133 @@ namespace Day11
             return layout[row][i];
         }
 
-        public static char LookDown(List<string> layout, int row, int col)
+        public static char LookRight(List<string> layout, int row, int col)
         {
-            if (row == layout.Count - 1)
+            int BOUNDRY = layout[row].Length - 1;
+
+            if (col == BOUNDRY)
             {
                 return 'Z';
             }
 
-            return OPEN;
+            int i = col;
+
+            while (++i <= BOUNDRY && layout[row][i] != OPEN && layout[row][i] != OCCUPIED)
+            {
+                continue;
+            }
+
+            if (i == BOUNDRY + 1)
+            {
+                return 'Z';
+            }
+
+            return layout[row][i];
         }
 
+        public static char LookUpLeft(List<string> layout, int row, int col)
+        {
+            int TOP_BOUNDRY = 0;
+            int LEFT_BOUNDRY = 0;
+
+            if (col == LEFT_BOUNDRY || row == TOP_BOUNDRY)
+            {
+                return 'Z';
+            }
+
+            int i = col;
+            int x = row;
+
+            while (--i >= LEFT_BOUNDRY && --x >= TOP_BOUNDRY  && layout[x][i] != OPEN && layout[x][i] != OCCUPIED)
+            {
+                continue;
+            }
+
+            if (i == LEFT_BOUNDRY - 1 || x == TOP_BOUNDRY - 1)
+            {
+                return 'Z';
+            }
+
+            return layout[x][i];
+        }
+
+        public static char LookUpRight(List<string> layout, int row, int col)
+        {
+            int TOP_BOUNDRY = 0;
+            int RIGHT_BOUNDRY = layout[row].Length - 1;
+
+            if (col == RIGHT_BOUNDRY || row == TOP_BOUNDRY)
+            {
+                return 'Z';
+            }
+
+            int i = col;
+            int x = row;
+
+            while (++i <= RIGHT_BOUNDRY && --x >= TOP_BOUNDRY  && layout[x][i] != OPEN && layout[x][i] != OCCUPIED)
+            {
+                continue;
+            }
+
+            if (i == RIGHT_BOUNDRY + 1 || x == TOP_BOUNDRY - 1)
+            {
+                return 'Z';
+            }
+
+            return layout[x][i];
+        }
+
+        public static char LookDownLeft(List<string> layout, int row, int col)
+        {
+            int BOTTOM_BOUNDRY = layout.Count - 1;
+            int LEFT_BOUNDRY = 0;
+
+            if (col == LEFT_BOUNDRY || row == BOTTOM_BOUNDRY)
+            {
+                return 'Z';
+            }
+
+            int i = col;
+            int x = row;
+
+            while (--i >= LEFT_BOUNDRY && ++x <= BOTTOM_BOUNDRY  && layout[x][i] != OPEN && layout[x][i] != OCCUPIED)
+            {
+                continue;
+            }
+
+            if (i == LEFT_BOUNDRY - 1 || x == BOTTOM_BOUNDRY + 1)
+            {
+                return 'Z';
+            }
+
+            return layout[x][i];
+        }
+
+        public static char LookDownRight(List<string> layout, int row, int col)
+        {
+            int BOTTOM_BOUNDRY = layout.Count - 1;
+            int RIGHT_BOUNDRY = layout[row].Length - 1;
+
+            if (col == RIGHT_BOUNDRY || row == BOTTOM_BOUNDRY)
+            {
+                return 'Z';
+            }
+
+            int i = col;
+            int x = row;
+
+            while (++i <= RIGHT_BOUNDRY && ++x <= BOTTOM_BOUNDRY  && layout[x][i] != OPEN && layout[x][i] != OCCUPIED)
+            {
+                continue;
+            }
+
+            if (i == RIGHT_BOUNDRY + 1 || x == BOTTOM_BOUNDRY + 1)
+            {
+                return 'Z';
+            }
+
+            return layout[x][i];
+        }
         public static string GetNewSeatAssignmentsForRow(List<string> rows)
         {
             string result = string.Empty;
@@ -161,36 +410,7 @@ namespace Day11
                 seatBlock.Add(i == 0 ? 'Z' : thisRow[i-1]);
                 seatBlock.Add(i == 0 ? 'Z' : belowRow[i-1]);
 
-                if (evalSeat == OPEN)
-                {
-                    // we can get cute later, for now just remove the garbage squares
-                    var cleanBlock = seatBlock.Where(x => x != 'Z' && x != '.').ToList();
-
-                    if (cleanBlock.All(x => x == OPEN))
-                    {
-                        result += OCCUPIED; // If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
-                    }
-                    else
-                    {
-                        result += OPEN;  // Otherwise, the seat's state does not change.
-                    }
-                }
-                else if (evalSeat == FLOOR)
-                {
-                    result += FLOOR;
-                }
-                else
-                {
-                    // If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
-                    if (seatBlock.Where(x => x == OCCUPIED).Count() > 3)
-                    {
-                        result += OPEN;
-                    }
-                    else
-                    {
-                        result += evalSeat;
-                    }
-                }
+                result += EvaluateSeat(seatBlock, evalSeat);
             }
 
             return result;
