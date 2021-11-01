e98c9c1980822aa4c1fb907f8c2ef42b64b3bba2
Jesse Quijano <jesse@quijano.net>
2021-10-31 22:39:46 -0700

    move directions to constants, add tests

---
+++ Advent2020.Tests/Day12Tests.Rotation.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Advent2020.Tests
+{
+    public class Rotation
+    {
+        [Fact]
+        public void Placeholder()
+        {
+            Assert.True(false);
+        }
+    }
+}
