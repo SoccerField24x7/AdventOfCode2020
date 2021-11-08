6d3468762b315900fa0ed7b5ccfed12a4b13d1d8
Jesse Quijano <jesse@quijano.net>
2021-11-07 20:24:27 -0800

    move logic to constants and FerryMover

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -11,17 +11,8 @@ namespace Day12
         {
             var instructions = FileHelper.GetFileContents<string>("./data/data.txt");
 
-            const char MOVE_NORTH = 'N';
-            const char MOVE_EAST = 'E';
-            const char MOVE_SOUTH = 'S';
-            const char MOVE_WEST = 'W';
-            const char MOVE_FORWARD = 'F';
-            const char ROTATE_RIGHT = 'R';
-            const char ROTATE_LEFT = 'L';
-
             int currentDirection = Direction.EAST;
-            int distanceVertically = 0;
-            int distanceHorizontally = 0;
+            ManhattanLocation position = new();
 
             foreach (string instruction in instructions)
             {
@@ -31,30 +22,25 @@ namespace Day12
                 // brute force it with a switch for now
                 switch(directive)
                 {
-                    case MOVE_NORTH:
-                        distanceVertically += amount;
+                    case Directive.MOVE_NORTH:
+                        position.VerticalPosition += amount;
                         break;
-                    case MOVE_SOUTH:
-                        distanceVertically -= amount;
+                    case Directive.MOVE_SOUTH:
+                        position.VerticalPosition -= amount;
                         break;
-                    case MOVE_EAST:
-                        distanceHorizontally += amount;
+                    case Directive.MOVE_EAST:
+                        position.HorizontalPosition += amount;
                         break;
-                    case MOVE_WEST:
-                        distanceHorizontally -= amount;
+                    case Directive.MOVE_WEST:
+                        position.HorizontalPosition -= amount;
                         break;
-                    case MOVE_FORWARD:
-                        if (currentDirection == Direction.EAST)
-                            distanceHorizontally += amount;
-                        if (currentDirection == Direction.WEST)
-                            distanceHorizontally -= amount;
-                        if (currentDirection == Direction.NORTH)
-                            distanceVertically += amount;
-                        if (currentDirection == Direction.SOUTH)
-                            distanceVertically -= amount;
+
+                    case Directive.MOVE_FORWARD:
+                        position = FerryMover.MoveForward(position, currentDirection, amount);
                         break;
-                    case ROTATE_RIGHT:
-                    case ROTATE_LEFT:
+
+                    case Directive.ROTATE_RIGHT:
+                    case Directive.ROTATE_LEFT:
                         currentDirection = FerryMover.RotateShip(currentDirection, directive, amount);
 
                         break;
@@ -63,7 +49,7 @@ namespace Day12
                 }
             }
 
-            Console.WriteLine(Math.Abs(distanceHorizontally) + Math.Abs(distanceVertically));
+            Console.WriteLine(Math.Abs(position.HorizontalPosition) + Math.Abs(position.VerticalPosition));
         }
     }
 }
