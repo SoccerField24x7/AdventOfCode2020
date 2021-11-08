d4402ce0b589343e1fec51ac97bbedfca947febe
Jesse Quijano <jesse@quijano.net>
2021-11-07 20:24:01 -0800

    move the "move forward" logic to ferry class

--- Day12/FerryMover.cs
+++ Day12/FerryMover.cs
@@ -63,5 +63,49 @@ namespace Advent2020.Day12
 
             return currentDirection;
         }
+
+        // public static int MoveDirectionally(ManhattanLocation position, char direction, int amount, int multiplier = 1)
+        // {
+        //     switch (direction)
+        //     {
+        //         case Directive.MOVE_NORTH:
+        //             distanceVertically += amount;
+        //             break;
+        //         case Directive.MOVE_SOUTH:
+        //             distanceVertically -= amount;
+        //             break;
+        //         case Directive.MOVE_EAST:
+        //             distanceHorizontally += amount;
+        //             break;
+        //         case Directive.MOVE_WEST:
+        //             distanceHorizontally -= amount;
+        //             break;
+        //     }
+        // }
+
+        public static ManhattanLocation MoveForward(ManhattanLocation position, int direction, int amount, int multiplier = 1)
+        {
+            ManhattanLocation endingPosition = new(position.HorizontalPosition, position.VerticalPosition);
+
+            switch (direction)
+            {
+                case Direction.EAST:
+                    endingPosition.HorizontalPosition += amount * multiplier;
+                    break;
+                case Direction.WEST:
+                    endingPosition.HorizontalPosition -= amount * multiplier;
+                    break;
+                case Direction.NORTH:
+                    endingPosition.VerticalPosition += amount * multiplier;
+                    break;
+                case Direction.SOUTH:
+                    endingPosition.VerticalPosition -= amount * multiplier;
+                    break;
+                default:
+                    throw new Exception("Invalid move direction.");                            
+            }
+
+            return endingPosition;
+        }
     }
 }
