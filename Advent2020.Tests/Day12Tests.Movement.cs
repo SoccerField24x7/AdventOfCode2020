66056944984641204b8710e1833383e660529a5a
Jesse Quijano <jesse@quijano.net>
2021-11-09 20:13:53 -0800

    updated unit tests

--- Advent2020.Tests/Day12Tests.Movement.cs
+++ Advent2020.Tests/Day12Tests.Movement.cs
@@ -11,12 +11,48 @@ namespace Advent2020.Tests
         {
             ManhattanLocation waypoint = new(10, 1);
             ManhattanLocation ship = new();
-            int facing = Direction.EAST;
             
-            var position = FerryMover.MoveForward(waypoint, facing, 10, 10);
+            ship = FerryMover.MoveToWaypoint(waypoint, ship, 10);
 
-            Assert.Equal(100, position.HorizontalPosition);
-            Assert.Equal(10, position.VerticalPosition);
+            Assert.Equal(100, ship.HorizontalPosition);
+            Assert.Equal(10, ship.VerticalPosition);
+
+            waypoint.HorizontalPosition = 10;
+            waypoint.VerticalPosition = 1;
+
+            waypoint = FerryMover.MoveDirectionally(waypoint, 'N', 3);
+
+            Assert.Equal(10, waypoint.HorizontalPosition);
+            Assert.Equal(4, waypoint.VerticalPosition);
+
+            ship = FerryMover.MoveToWaypoint(waypoint, ship, 7);
+
+            Assert.Equal(170, ship.HorizontalPosition);
+            Assert.Equal(38, ship.VerticalPosition);
+
+
+        }
+
+        [Fact]
+        public void WaypointRotateRightFrom90()
+        {
+            ManhattanLocation waypoint = new(10, 4);
+
+            waypoint = FerryMover.RotateWaypoint(waypoint, Directive.ROTATE_RIGHT, 90);
+
+            Assert.Equal(waypoint.HorizontalPosition, 4);
+            Assert.Equal(waypoint.VerticalPosition, -10);
+        }
+
+        [Fact]
+        public void WaypointRotateLeft90From90()
+        {
+            ManhattanLocation waypoint = new(10, 4);
+
+            waypoint = FerryMover.RotateWaypoint(waypoint, Directive.ROTATE_LEFT, 90);
+
+            Assert.Equal(waypoint.HorizontalPosition, -4);
+            Assert.Equal(waypoint.VerticalPosition, 10);
         }
     }
 }
